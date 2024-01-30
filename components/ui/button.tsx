a5a1d5426f6ad477d48222cc41e09f4fc1740008
chanakyha <vetrichanakyha2003@gmail.com>
2024-01-31 02:09:51 +0530

    changes done

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-gradient-to-r from-ui-primary to-ui-secondary text-primary-foreground font-semibold hover:bg-ui-primary/90",
+          "gradient text-primary-foreground font-semibold hover:bg-ui-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
