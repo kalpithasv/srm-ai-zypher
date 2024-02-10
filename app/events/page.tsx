db5b5a0fee8d391129f6d47eea9b650423d4f8cf
chanakyha <vetrichanakyha2003@gmail.com>
2024-02-10 15:55:44 +0530

    Added 👍

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -42,7 +42,7 @@ const EventsPage = async () => {
         <Image
           alt="event-banner"
           src={
-            "https://firebasestorage.googleapis.com/v0/b/srm-ai-zypher.appspot.com/o/WhatsApp%20Image%202024-02-06%20at%2015.47.53.jpeg?alt=media&token=7831ebd3-3b2b-4001-9e2e-e860905ca35d"
+            "https://firebasestorage.googleapis.com/v0/b/srm-ai-zypher.appspot.com/o/PRINT-01.jpg?alt=media&token=72a23dae-f0b8-4d79-9418-b817dbe874f6"
           }
           width={1920}
           height={1080}
