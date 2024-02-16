ca4dc03bf6a34fb5c6fd580c5cbfd35094a56125
chanakyha <vetrichanakyha2003@gmail.com>
2024-02-16 20:43:39 +0530

    sponsors marquee added

--- components/home/Sponsors.tsx
+++ components/home/Sponsors.tsx
@@ -2,7 +2,12 @@ import Marquee from "react-fast-marquee";
 import Image, { StaticImageData } from "next/image";
 
 const Sponsors = () => {
-  const sponsorImages: StaticImageData[] = [];
+  const sponsorImages: StaticImageData[] = [
+    require("@/images/sponsors/A R PNG.png"),
+    require("@/images/sponsors/Original.png"),
+    require("@/images/sponsors/WhatsApp Image 2024-02-16 at 11.13.53.jpeg"),
+    require("@/images/sponsors/WhatsApp Image 2024-02-16 at 11.44.20.jpeg"),
+  ];
   return (
     <div className="bg-ui-lightBg py-4 px-4 md:px-16">
       <h1 className="text-center text-2xl text-black font-semibold underline underline-offset-8 decoration-ui-primary">
@@ -16,15 +21,19 @@ const Sponsors = () => {
         pauseOnHover
         autoFill
       >
-        <div className="flex items-center gap-8 mt-4 mx-4">
+        <div className="flex items-stretch gap-8 mt-4 mx-4">
           {sponsorImages.map((image: StaticImageData, index: number) => {
             return (
-              <Image
+              <div
+                className="bg-white px-4 py-2 flex items-center rounded-md shadow-sm"
                 key={index}
-                alt="sponsor-image"
-                src={image}
-                className="h-fit w-20 cursor-pointer hover:scale-110 transition-all duration-300 ease-in-out object-contain"
-              />
+              >
+                <Image
+                  alt="sponsor-image"
+                  src={image}
+                  className="h-fit  w-20 cursor-pointer hover:scale-110 transition-all duration-300 ease-in-out object-contain"
+                />
+              </div>
             );
           })}
         </div>
